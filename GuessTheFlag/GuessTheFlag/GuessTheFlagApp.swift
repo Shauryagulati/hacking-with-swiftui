ea17974ce94aa54b1e27e7b56138537f2a75c33d
Shaurya Gulati <Sgulati1020@gmail.com>
2021-08-01 18:51:48 +0530

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Shaurya Gulati on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
